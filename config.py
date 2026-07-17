f2567f81f6794a5594e719f6b1a182f4cca361e5
Dillan Ramos <Ramos.dillan@uces.edu.co>
2026-07-16 22:55:17 -0500

    fix: agregar variables de Cloudinary a Config

--- config.py
+++ config.py
@@ -35,3 +35,8 @@ class Config:
 
     # OpenAI
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+    # Cloudinary
+    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
